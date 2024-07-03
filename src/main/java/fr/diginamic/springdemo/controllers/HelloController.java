2a4400a421fadfca8f1d026cfa9447056ccab0ec
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-07-03 11:41:34 +0200

    add: Exercise 2 Injection Dependency HelloService

--- src/main/java/fr/diginamic/springdemo/controllers/HelloController.java
+++ src/main/java/fr/diginamic/springdemo/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package fr.diginamic.springdemo.controllers;
 
+import fr.diginamic.springdemo.beans.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Autowired
+    private HelloService service;
+
     @GetMapping
     public String helloWorld() {
-        return "Hello World!";
+        return service.salutations();
     }
 }
