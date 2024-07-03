7a953832ee9c68a7a8020387b71878ce129063aa
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-07-03 11:47:06 +0200

    fix: Space Indentation HelloService.salutations

--- src/main/java/fr/diginamic/springdemo/beans/HelloService.java
+++ src/main/java/fr/diginamic/springdemo/beans/HelloService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
-        public String salutations() {
-            return "Je suis la classe de service et je vous dis Bonjour !";
-        }
+    public String salutations() {
+        return "Je suis la classe de service et je vous dis Bonjour !";
+    }
 }
