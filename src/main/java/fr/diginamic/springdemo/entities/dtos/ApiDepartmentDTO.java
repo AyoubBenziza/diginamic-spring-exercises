5d6c49d24918c10851db32b685fcb451be48ce03
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-07-08 16:01:07 +0200

    add: Javadoc

--- src/main/java/fr/diginamic/springdemo/entities/dtos/ApiDepartmentDTO.java
+++ src/main/java/fr/diginamic/springdemo/entities/dtos/ApiDepartmentDTO.java
@@ -3,33 +3,28 @@ package fr.diginamic.springdemo.entities.dtos;
 /**
  * ApiDepartmentDTO is a class used to represent the Department entity in the API
  * It is used to serialize the Department entity into a JSON object
+ *
  * @author AyoubBenziza
  */
-public class ApiDepartmentDTO {
-    private String nom;
-    private String code;
+public record ApiDepartmentDTO(String nom, String code) {
 
-    public ApiDepartmentDTO() {
-    }
-
-    public ApiDepartmentDTO(String nom, String code) {
-        this.nom = nom;
-        this.code = code;
-    }
-
-    public String getNom() {
+    /**
+     * Get the name of the department
+     *
+     * @return the name of the department
+     */
+    @Override
+    public String nom() {
         return nom;
     }
 
-    public void setNom(String name) {
-        this.nom = name;
-    }
-
-    public String getCode() {
+    /**
+     * Get the code of the department
+     *
+     * @return the code of the department
+     */
+    @Override
+    public String code() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 }
